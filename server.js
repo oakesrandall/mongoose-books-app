84572288e6cbd65ace1745b27caf50775d683641
David_Randall <oakesrandall@gmail.com>
2017-07-25 14:02:40 -0600

    finished sprint 2, need to work on show

--- server.js
+++ server.js
@@ -38,10 +38,12 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  db.Book.find(function(err, books){
-    if (err) { return console.log("index error: " + err); }
-    res.json(books);
-  });
+  db.Book.find()
+    .populate('author')
+    .exec(function(err, books) {
+      if (err) {return console.log('indes error: ' + err);}
+      res.json(books);
+    });
 });
 // get one book
 app.get('/api/books/:id', function (req, res) {
@@ -58,10 +60,25 @@ app.get('/api/books/:id', function (req, res) {
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  books.push(newBook);
-  res.json(newBook);
+  var newBook = new db.Book({
+    title: req.body.title,
+    image: req.body.image,
+    releaseDate: req.body.releaseDate,
+  });
+
+  // this code will only add an author to a book if the author already exists
+  db.Author.findOne({name: req.body.author}, function(err, author){
+    newBook.author = author;
+    // add newBook to database
+    newBook.save(function(err, book){
+      if (err) {
+        return console.log("create error: " + err);
+      }
+      console.log("created ", book.title);
+      res.json(book);
+    });
+  });
+
 });
 
 // update book
